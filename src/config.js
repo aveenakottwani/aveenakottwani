954606341392048c0978fa8da056dc473a9e24fe
Aveena Kottwani <1703aveena@gmail.com>
2020-07-26 12:48:12 -0700

    update names and logoes

--- src/config.js
+++ src/config.js
@@ -1,38 +1,38 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'Aveena Kottwani | Software Engineer',
   siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    'Aveena Kottwani is a software engineer specializes in building backend web service devlopment, Machine learning , big data analytics applications, and cloud infrastructure services.',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
-  siteUrl: 'https://brittanychiang.com',
+    'Aveena Kottwani, Aveena , Kottwani, aveenakottwani, software engineer, back-end engineer, software developer, JAVA, Stony Brook',
+  siteUrl: 'https://aveenakottwani.com',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
   name: 'Brittany Chiang',
   location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  email: 'aveenakottwani@gmail.com',
+  github: 'https://github.com/aveenakottwani',
+  twitterHandle: '@kottavu',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/aveenakottwani',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/aveena-kottwani/',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://leetcode.com/aveena/',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/aveenakott/',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/kottavu',
     },
   ],
 
